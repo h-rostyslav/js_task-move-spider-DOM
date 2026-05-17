ae8e454c679b3da1f5528d8cd804de2f6630fd4f
Rostyslav` <hrostyslav@ukr.net`>
2026-05-18 00:08:47 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,35 +4,33 @@ const wall = document.querySelector('.wall');
 const spider = wall.querySelector('.spider');
 
 wall.addEventListener('click', (e) => {
+  const clickX = e.clientX;
+  const clickY = e.clientY;
+
+  const wallRect = wall.getBoundingClientRect();
+  const wallBorder = parseInt(getComputedStyle(wall).borderWidth, 10) || 0;
+
   const spiderWidth = spider.offsetWidth;
   const spiderHeight = spider.offsetHeight;
 
-  const wallWidth = wall.offsetWidth;
-  const wallHeight = wall.offsetHeight;
+  let spiderX = clickX - wallRect.left - wallBorder - spiderWidth / 2;
+  let spiderY = clickY - wallRect.top - wallBorder - spiderHeight / 2;
 
-  const wallRect = wall.getBoundingClientRect();
-
-  let clickX = e.clientX - wallRect.left;
-  let clickY = e.clientY - wallRect.top;
-
-  let spiderX = clickX - spiderWidth / 2;
-  let spiderY = clickY - spiderHeight / 2;
-  
+  const maxLeft = wall.clientWidth - spiderWidth;
+  const maxTop = wall.clientHeight - spiderHeight;
 
   if (spiderX < 0) {
     spiderX = 0;
-  } else if (spiderX > wallWidth - spiderWidth) {
-    spiderX = wallWidth - spiderWidth;
+  } else if (spiderX > maxLeft) {
+    spiderX = maxLeft;
   }
 
   if (spiderY < 0) {
     spiderY = 0;
-  } 
-  
-  if (spiderY > wallHeight - spiderHeight) {
-    spiderY = wallHeight - spiderHeight;
+  } else if (spiderY > maxTop) {
+    spiderY = maxTop;
   }
 
-  spider.style.left = spiderX + 'px';
-  spider.style.top = spiderY + 'px';
+  spider.style.left = `${spiderX}px`;
+  spider.style.top = `${spiderY}px`;
 });
